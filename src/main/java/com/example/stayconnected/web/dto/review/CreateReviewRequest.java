156cc7b257d929222e006d7f4134aa2356283514
JekoGyulev <veracvetanova7@gmail.com>
2025-09-22 19:56:39 +0300

    feat: add DTO for review

---
+++ src/main/java/com/example/stayconnected/web/dto/review/CreateReviewRequest.java
@@ -0,0 +1,29 @@
+package com.example.stayconnected.web.dto.review;
+
+public class CreateReviewRequest {
+    private String comment;
+    private int rating;
+
+    public CreateReviewRequest() {}
+
+    public CreateReviewRequest(String comment, int rating) {
+        this.comment = comment;
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
