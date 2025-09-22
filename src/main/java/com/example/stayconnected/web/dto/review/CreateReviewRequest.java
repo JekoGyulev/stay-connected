ee48206cb5fabe43503c41bcfee93a9f462b42f1
JekoGyulev <veracvetanova7@gmail.com>
2025-09-22 20:02:05 +0300

    fix: add validation for CreateReviewRequest

--- src/main/java/com/example/stayconnected/web/dto/review/CreateReviewRequest.java
+++ src/main/java/com/example/stayconnected/web/dto/review/CreateReviewRequest.java
@@ -1,7 +1,14 @@
 package com.example.stayconnected.web.dto.review;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class CreateReviewRequest {
+    @NotBlank
     private String comment;
+    @Min(1)
+    @Max(5)
     private int rating;
 
     public CreateReviewRequest() {}
