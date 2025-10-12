f9cae3ced2460ca5903e861549ca56603c5e4b8f
JekoGyulev <veracvetanova7@gmail.com>
2025-10-12 11:48:33 +0300

    chore: integrate HandlerInterceptor

---
+++ src/main/java/com/example/stayconnected/security/SessionCheckInterceptor.java
@@ -0,0 +1,41 @@
+package com.example.stayconnected.security;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Set;
+
+@Component
+public class SessionCheckInterceptor implements HandlerInterceptor {
+
+    private static final Set<String> UNAUTHENTICATED_ENDPOINTS = Set.of("/", "/register", "/login");
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        if (UNAUTHENTICATED_ENDPOINTS.contains(request.getPathInfo())) {
+            return true;
+        }
+
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            response.sendRedirect("/login");
+            return false;
+        }
+
+        Object userId = session.getAttribute("userId");
+
+        if (userId == null) {
+            session.invalidate();
+            response.sendRedirect("/login");
+            return false;
+        }
+
+        return true;
+    }
+}
