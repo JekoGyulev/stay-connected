aa606c737da54a777a068e22639af5606e704b07
JekoGyulev <veracvetanova7@gmail.com>
2025-10-12 11:56:11 +0300

    feat: register HandlerInterceptor

---
+++ src/main/java/com/example/stayconnected/security/WebConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.stayconnected.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private final SessionCheckInterceptor interceptor;
+
+    @Autowired
+    public WebConfiguration(SessionCheckInterceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/images/**", "/css/**");
+    }
+}
