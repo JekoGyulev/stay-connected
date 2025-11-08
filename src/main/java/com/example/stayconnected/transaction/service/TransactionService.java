e11f100d6461ee7b90775b64682335d12147ab86
JekoGyulev <zhekogyulev@gmail.com>
2025-11-08 09:55:45 +0200

    feat: create TransactionService and TransactionServiceImpl that hold business logic of the domain entity

---
+++ src/main/java/com/example/stayconnected/transaction/service/TransactionService.java
@@ -0,0 +1,4 @@
+package com.example.stayconnected.transaction.service;
+
+public interface TransactionService {
+}
---
+++ src/main/java/com/example/stayconnected/transaction/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.stayconnected.transaction.service.impl;
+
+import com.example.stayconnected.transaction.service.TransactionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+}
