4802738a0f7395ccd055d261d89346ffd298cc0c
JekoGyulev <zhekogyulev@gmail.com>
2025-12-01 12:35:02 +0200

    refactor : remove unused imports

--- src/main/java/com/example/stayconnected/web/controller/ReservationController.java
+++ src/main/java/com/example/stayconnected/web/controller/ReservationController.java
@@ -8,13 +8,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
-import java.util.UUID;
 
 @Controller
 @RequestMapping("/reservations")
