86e12c393d174e646ac4f89c3b4b353848632460
JekoGyulev <165460153+JekoGyulev@users.noreply.github.com>
2025-12-28 23:04:29 +0200

    feat: Add theme switching with global dark mode support

---
+++ src/main/resources/static/js/darkmode.js
@@ -0,0 +1,52 @@
+
+    const root = document.documentElement;
+    const body = document.getElementsByTagName("body")[0];
+
+    const themeOptions = document.querySelectorAll('input[name="theme"]');
+
+
+    function applySystemTheme() {
+        const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        body.classList.toggle("dark-mode", prefersDarkMode);
+    }
+
+
+    themeOptions.forEach(option => {
+
+        option.addEventListener("change", (e) => {
+            const selectedTheme = e.target.value;
+
+            if (selectedTheme === "dark") {
+                root.classList.add("dark-mode");
+            } else if (selectedTheme === "light") {
+                root.classList.remove("dark-mode");
+            } else if (selectedTheme === "system") {
+                applySystemTheme();
+            }
+
+
+            localStorage.setItem("theme", selectedTheme);
+        });
+
+    });
+
+
+    const preference = localStorage.getItem("theme") || "light";
+
+    const savedRadio = document.querySelector(
+        `input[name="theme"][value="${preference}"]`
+    );
+
+    if (savedRadio) {
+        savedRadio.checked = true;
+    }
+
+    if (preference === "dark") {
+        root.classList.add("dark-mode");
+
+    } else if (preference === "system") {
+        applySystemTheme();
+    }
+
+
+
