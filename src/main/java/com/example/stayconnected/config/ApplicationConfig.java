9d00d6d9976fe278c539a4b012aed93e42657b2a
JekoGyulev <165460153+JekoGyulev@users.noreply.github.com>
2026-01-06 23:33:13 +0200

    refactor: fix some untranslated elements

--- src/main/java/com/example/stayconnected/config/ApplicationConfig.java
+++ src/main/java/com/example/stayconnected/config/ApplicationConfig.java
@@ -6,13 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 @Configuration
 public class ApplicationConfig {
-
-    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Bean
     public PasswordEncoder passwordEncoder() {
