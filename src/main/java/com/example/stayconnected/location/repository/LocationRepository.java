73a3e27ba7ccc2824a1fa99776f51fe699b00632
JekoGyulev <165460153+JekoGyulev@users.noreply.github.com>
2026-01-27 21:34:20 +0200

    fix: fix a bug where duplicate location(duplicate by country, city and address at the same time) was saved to the database (created)

--- src/main/java/com/example/stayconnected/location/repository/LocationRepository.java
+++ src/main/java/com/example/stayconnected/location/repository/LocationRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -21,4 +22,6 @@ public interface LocationRepository extends JpaRepository<Location, UUID> {
             "ORDER BY COUNT(l.city) DESC " +
             "LIMIT 4")
     List<CityStatsDTO> findTop4PopularDestinations();
+
+    Optional<Location> findByCountryAndCityAndAddress(String country, String city, String address);
 }
